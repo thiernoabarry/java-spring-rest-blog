872bb3771f68cac4e4a449251af2b264a79b7b84
thiernoabarry <thierno.abarry@yahoo.fr>
2021-02-17 11:40:35 +0100

    Completed module

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,9 +4,10 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public interface PostRepository extends JpaRepository<Post,Long>{
 
     @RestResource(rel = "contains-title",path = "containsTitle")
